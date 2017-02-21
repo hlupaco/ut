3835a07bc0dc633d8a189ebdaee6a37d75932581
Ubuntu <ubuntu@ip-172-31-12-29.eu-central-1.compute.internal>
2017-02-21 12:34:23 +0000

    region list

--- GetStatsForEC2.sh
+++ GetStatsForEC2.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 #Usage: sh $0 2>/dev/null
 
-rand=10
+rand="1"
 TEMPDIR="/tmp/MM/$rand"
-CONFDIR="/home/ubuntu/Task_cron_ec_stats"
-
-#region_list="$CONFDIR/regions"
 
 while [ -d $TEMPDIR ] ; do
-  rand=$(echo "$rand * $$" | bc)
+  rand=$(echo "($rand * $$) % 10000" | bc)
   TEMPDIR="/tmp/MM/$rand"
 done
+
 mkdir /tmp/MM 2>/dev/null
 mkdir "$TEMPDIR"
 
 echo Using $TEMPDIR 1>&2
 
-#for reg in $(cat $region_list) ; do
-for reg in us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 eu-west-1 \
-           eu-central-1 eu-west-2 ap-northeast-1 ap-northeast-2 ap-southeast-1 \
-           ap-southeast-2 ap-south-1 sa-east-1 ; do
+for reg in $(aws ec2 describe-regions --query 'Regions[].{Name:RegionName}' \
+           --output text --region eu-west-1) ; do
 
   aws ec2 describe-instances --region "$reg" | grep InstanceId | cut -d'"' -f4 \
       > $TEMPDIR/instances
@@ -33,12 +29,12 @@ for reg in us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 eu-west-1 \
     echo 1>&2
     echo "InstanceId: $inst_id" 1>&2
 
+    echo -n "$inst_id" >> $TEMPDIR/output
+
     aws ec2 describe-instances --region "$reg" --instance-id "$inst_id" \
         | egrep 'InstanceType|AvailabilityZone|"Name": "|PublicDnsName
                  |PublicIpAddress|LaunchTime' \
         | tr -d ' ' | tr -d '"' | tr -d ',' | sort -u >> $TEMPDIR/$inst_id
-
-    echo -n "$inst_id" >> $TEMPDIR/output
 
     for stat in AvailabilityZone InstanceType LaunchTime Name PublicDnsName \
                 PublicIpAddress ; do
